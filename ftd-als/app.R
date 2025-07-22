418e4825e624eab4e09bf32de09cd7844bf965d7
Damien de Vienne <damien.de-vienne@univ-lyon1.fr>
2025-07-22 19:07:25 +0200

    v1 with pdf output

--- ftd-als/app.R
+++ ftd-als/app.R
@@ -1,10 +1,11 @@
 require(shiny)
 library(shinyjs) # For writing messages to console
 library(plotly) # For interactive plots
-#library(dplyr)
 library(bslib)
+library(rmarkdown) #for pdf report
+library(knitr) #for pdf report
 
-#df_onset <- readRDS("df_onset.rds")
+
 df_onset <- read.csv("df_onset.csv", stringsAsFactors = FALSE)
 ###########################
 ## FUNCTIONS (JULY 2025) ##
@@ -91,19 +92,79 @@ pSICKn_grandchild <- function(x, pi_t2,pi_t2_n, pi_t1) { #pi_t2_n is the penetra
       )
   }
 
+  make_combined_plot <- function(df, age_input, carrier_prob, sick_prob, nyears) {
+    plot_ly(data = df, type = 'scatter', mode = 'lines') %>%
+      add_trace(x = ~Age, y = ~PvariantWhenHealthy,
+                name = "Carrier probability",
+                line = list(color = "steelblue")) %>%
+      add_trace(x = ~Age, y = ~PsickInFuture,
+                name = paste("Disease risk (next",nyears, " years)"),
+                line = list(color = "firebrick")) %>%
+      layout(
+        margin = list(l = 60, r = 10, t = 10, b = 60),
+        xaxis = list(title = "Age of the consultand"),
+        yaxis = list(title = "Estimated probability"),
+        shapes = list(
+          list(type = "line", x0 = age_input, x1 = age_input, y0 = 0, y1 = 1,
+              line = list(dash = "dot", color = "gray")),
+          list(type = "line", x0 = 0, x1 = 100, y0 = carrier_prob, y1 = carrier_prob,
+              line = list(dash = "dot", color = "steelblue")),
+          list(type = "line", x0 = 0, x1 = 100, y0 = sick_prob, y1 = sick_prob,
+              line = list(dash = "dot", color = "firebrick"))
+        ),
+        legend = list(
+          x = 1,
+          y = 1,
+          xanchor = "right",
+          yanchor = "top",
+          bgcolor = "rgba(255,255,255,0.5)",
+          bordercolor = "gray",
+          borderwidth = 1
+        )
+      ) %>%
+      config(displayModeBar = FALSE, editable = FALSE)
+  }
+
+
 ####################
 ## USER INTERFACE ##
 ####################
 
-library(shiny)
-library(shinyjs)
-library(bslib)
-
 ui <- fluidPage(
+
   useShinyjs(),
+
+  tags$head(
+    tags$style(HTML("
+      html, body {
+        height: 100%;
+        margin: 0;
+      }
+
+      .container-fluid {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+      }
+
+      .main-content {
+        flex: 1;
+      }
+      .summary {
+        border: 1px solid rgba(0, 0, 0, 0.176);
+        padding: 0px 16px 16px 16px;
+      } 
+
+      footer {
+        margin-top: auto;
+      }
+
+    "))
+  ),
 
   # Bootstrap Theme
   theme = bs_theme(version = 5, bootswatch = "yeti"),  # Change here for different looks
+
 
   # Header
 
@@ -116,235 +177,124 @@ ui <- fluidPage(
           class = "small",
           HTML(paste0(
             'Source code on <a href="https://github.com/damiendevienne/ftd-als" target="_blank" class="text-white text-decoration-underline">GitHub</a> &nbsp;|&nbsp; ',
-            'Licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank" class="text-white text-decoration-underline">MIT License</a>'
+            'Licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank" class="text-white text-decoration-underline">MIT License</a> &nbsp;|&nbsp; ',
+            'Contact <a href="mailto:dominique.de-vienne@inrae.fr" target="_blank" class="text-white text-decoration-underline">Dominique de Vienne</a>'
           ))
         )
       )
     ),
 
   # Main layout
-  fluidRow(
-    column(width = 1),  # Left margin
+  div(class="main-content",
+    fluidRow(
+      column(width = 2),  # Left margin
 
-    column(width = 10,
-      # Title and description
-      fluidRow(
-        column(12,
-          tags$p("This application estimates the probability of carrying the C9orf72 repeat expansion mutation for an unaffected first- or second-degree relative of an affected individual. Estimates are based on age-specific penetrance data (Murphy et al., 2017) and methods by de Vienne & de Vienne (in prep)."),
-          tags$p("For publication use, please cite:",
-                 tags$i("de Vienne D & de Vienne DM. In prep. Improving Genetic Counseling for C9orf72-ALS/FTD with Age-Based Risk Estimates."))
-        )
-      ),
-
-      # Inputs and Outputs
-      fluidRow(
-        column(width = 3,
-          wellPanel(
-            tags$h4("Input Parameters", class = "mb-3"),
-
-            radioButtons("relation", "Family relationship of the consultand to the affected individual:",
-                         choices = c("Child" = "child", "Grandchild" = "grandchild")),
-
-            numericInput("Penetrance", label = "Assumed disease penetrance:", value = 1, min = 0, max = 1, step = 0.05),
-
-            sliderInput("age", "Current age of the patient:", value = 40, min = 0, max = 100, step = 1),
-
-            conditionalPanel(
-              condition = "input.relation == 'grandchild'",
-              sliderInput("parent_age", "Age of the parent (child of the affected individual):", value = 70, min = 15, max = 100, step = 1),
-              tags$div(
-                class = "small text-muted",
-                "Age gap: ",
-                textOutput("age_gap", inline = TRUE),
-                " years"
-              )
-            ), 
-
-            numericInput("n_years", 
-              label = "Time frame for disease risk estimation (in years):", 
-              value = 10, 
-              min = 1, 
-              max = 100, 
-              step = 1)
+      column(width = 8,
+        # Title and description
+        fluidRow(
+          column(12,
+            tags$p("This application estimates the probability of carrying the C9orf72 repeat expansion mutation for an unaffected first- or second-degree relative of an affected individual. Estimates are based on age-specific penetrance data (Murphy et al., 2017) and methods by de Vienne & de Vienne (in prep)."),
+            tags$p("For publication use, please cite:",
+                  tags$i("de Vienne D & de Vienne DM. In prep. Improving Genetic Counseling for C9orf72-ALS/FTD with Age-Based Risk Estimates."))
           )
         ),
 
-        column(width = 9,
-          wellPanel(
-            tags$h4("Risk estimates visualization"),
+        # Inputs and Outputs
+        fluidRow(
+          div(class = "col-12 col-xl-4 mb-4",
+            wellPanel(
+              tags$h4("Input Parameters", class = "mb-3"),
 
-            fluidRow(
-              column(width = 6,
-                tags$h5("Probability of carrying the C9orf72 RE mutation"),
-                plotlyOutput("interactive_plot", height = "500px")
-              ),
-              column(width = 6,
-                uiOutput("dynamic_title_nyears"),
-                plotlyOutput("interactive_plot2", height = "500px")
-              )
+              radioButtons("relation", "Family relationship of the consultand to the affected individual:",
+                          choices = c("Child" = "child", "Grandchild" = "grandchild")),
+
+              numericInput("Penetrance", label = "Assumed disease penetrance:", value = 1, min = 0, max = 1, step = 0.05),
+
+              sliderInput("age", "Current age of the patient:", value = 40, min = 0, max = 100, step = 1),
+
+              conditionalPanel(
+                condition = "input.relation == 'grandchild'",
+                sliderInput("parent_age", "Age of the parent (child of the affected individual):", value = 70, min = 15, max = 100, step = 1),
+                tags$div(
+                  class = "small text-muted",
+                  "Age gap: ",
+                  textOutput("age_gap", inline = TRUE),
+                  " years"
+                )
+              ), 
+
+              numericInput("n_years", 
+                label = "Time frame for disease risk estimation (in years):", 
+                value = 10, 
+                min = 1, 
+                max = 100, 
+                step = 1)
             )
           ),
 
-          wellPanel(
-            tags$h4("Risk estimates summary", class = "mb-3"),
-            tags$p(tags$strong("Probability to carry the mutation: "),
-              textOutput("carrier_prob_final", inline = TRUE)),
-            tags$p(tags$strong("Probability to develop the disease: "),
-              textOutput("sick_prob_final", inline = TRUE))
+          div(class = "col-12 col-xl-4 mb-4",
+            wellPanel(
+              tags$h4("Risk estimates visualization"),
+              plotlyOutput("combined_plot", height = "500px")
+            )
+          ),
+
+          div(class = "col-12 col-xl-4 mb-4",
+            wellPanel(
+              div(class="summary",
+              tags$h4("Risk estimates summary", class = "mb-3"),
+
+              tags$label("Relationship to the affected individual:", textOutput("summary_relation", inline = TRUE)),
+              tags$br(),
+              tags$label("Current age of the consultand:", textOutput("summary_age", inline = TRUE)),
+              tags$br(),
+              tags$label("Assumed disease penetrance:", textOutput("summary_penetrance", inline = TRUE)),
+
+              conditionalPanel(
+                condition = "input.relation == 'grandchild'",
+                tags$label("Parent's age:", textOutput("summary_parent_age", inline = TRUE)),
+                tags$br(),
+                tags$label("Age gap (parent - consultand):", textOutput("summary_age_gap", inline = TRUE))
+              ),
+
+              #tags$label(tags$strong("Time frame for disease risk estimate:"), textOutput("summary_nyears", inline = TRUE)),
+
+              tags$hr(),
+
+              tags$label(tags$strong("Estimated probability of carrying the mutation:"), 
+                textOutput("carrier_prob_final", inline = TRUE)),
+              tags$br(),
+              tags$label(tags$strong("Estimated probability of developing the disease in the next ", 
+                textOutput("summary_nyears", inline = TRUE),
+                "years: "),
+                textOutput("sick_prob_final", inline = TRUE)
+              ), 
+              div(class="mt-4", style = "text-align: center;",
+                  downloadButton("download_report", "Download report (pdf)")
+              )
+            ))
+
+
+
+
+
           )
         )
-      )
-    ),
 
-    column(width = 1)  # Right margin
-  ),
+      ),
+
+      column(width = 2)  # Right margin
+    )
+  ),  # End of main content. NOTE this div allows for the footer to be at the bottom of the page
 
   # Footer
   tags$footer(
     class = "text-center py-3 border-top mt-4",
     HTML(paste0(
-      '<img src="logo_lbbe.svg" alt="LBBE logo" height="30" style="vertical-align: middle; margin-right: 10px;">',
-      '&nbsp; Contact: <a href="mailto:dominique.de-vienne@inrae.fr">dominique.de-vienne@inrae.fr</a>'
+      '<img src="logo_lbbe.svg" alt="LBBE logo" height="30" style="vertical-align: middle; margin-right: 10px;">'
     ))
   )
 )
-
-
-# ui <- fluidPage(
-#   useShinyjs(),
-
-#   # Custom CSS
-#   tags$style(HTML("
-#     body {
-#       background-color: #f8f9fa;
-#       font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-#       color: #333;
-#     }
-#     .control-label {
-#       font-size: 16px;
-#       font-weight: bold;
-#     }
-#     .results-panel {
-#       background-color: #f1f1f1;
-#       border: 1px solid #ddd;
-#       padding: 15px;
-#       border-radius: 6px;
-#       margin-top: 20px;
-#     }
-#     .result-label {
-#       font-weight: bold;
-#     }
-#     .well {
-#       background-color: #ffffff;
-#       border: 1px solid #ccc;
-#     }
-#     .plots-panel {
-#       background-color: #f0f0f0; 
-#     }
-#   ")),
-
-#   # Theme
-#   theme = bs_theme(version = 5, bootswatch = "darkly"),
-
-#   # Main layout
-#   fluidRow(
-#     column(width = 1),  # Left margin
-    
-#     column(width = 10,
-#       # Title and description
-#       fluidRow(
-#         column(12,
-#           tags$h2("Genetic Counseling for ALS/FTD", style = "text-align: center; margin-top: 20px;"),
-#           tags$h4("Carrier Probability Estimation for C9orf72 Repeat Expansion", style = "text-align: center; margin-bottom: 20px;"),
-#           tags$p("This application estimates the probability of carrying the C9orf72 repeat expansion mutation for an unaffected first- or second-degree relative of an affected individual. Estimates are based on age-specific penetrance data (Murphy et al., 2017) and formulae from de Vienne & de Vienne (in prep)."),
-#           tags$p("For publication use, please cite:", 
-#                  tags$i("de Vienne D & de Vienne DM. In prep. Improving Genetic Counseling for C9orf72-ALS/FTD with Age-Based Risk Estimates."))
-#         )
-#       ),
-
-#       # Inputs and Outputs
-#       fluidRow(
-#         column(width = 3,
-#           wellPanel(
-#             tags$h4("Input Parameters", style = "margin-bottom: 15px;"),
-
-#             radioButtons("relation", "Family relationship of the consultand to the affected individual:",
-#                          choices = c("Child" = "child", "Grandchild" = "grandchild")),
-
-#             numericInput("Penetrance", label = "Assumed disease penetrance:", value = 1, min = 0, max = 1, step = 0.05),
-
-#             sliderInput("age", "Current age of the patient:", value = 40, min = 0, max = 100, step = 1),
-
-#             conditionalPanel(
-#               condition = "input.relation == 'grandchild'",
-#               sliderInput("parent_age", "Age of the parent (child of the affected individual):", value = 70, min = 15, max = 100, step = 1),
-#               tags$div(
-#                 style = "font-size: 90%; color: #444;",
-#                 "Age gap: ",
-#                 textOutput("age_gap", inline = TRUE),
-#                 " years"
-#               )
-#             ), 
-
-#             numericInput("n_years", 
-#               label = "Time frame for disease risk estimation (in years):", 
-#               value = 10, 
-#               min = 1, 
-#               max = 100, 
-#               step = 1)
-#           )
-#         ),
-#         column(width = 9,
-#           wellPanel(class= "plots-panel",
-#             h4("Results"),
-#             fluidRow(
-#               column(width = 6,
-#                 h5("Probability of carrying the c9orf72 RE mutation"),
-#                 plotlyOutput("interactive_plot", height = "500px")
-#               ),
-#               column(width = 6,
-#                  uiOutput("dynamic_title_nyears"),
-# #                h5("Probability of developing the disease in the next N years"),
-#                 plotlyOutput("interactive_plot2", height = "500px")
-#               )
-#             )
-#           ),
-#           wellPanel(class = "results-panel",
-#             h4("Risk estimates summary", style = "margin-bottom: 15px;"),
-#             p(span(class = "result-label", "Probability to carry the mutation: "),
-#               textOutput("carrier_prob_final", inline = TRUE)),
-#             p(span(class = "result-label", "Probability to develop the disease: "),
-#               textOutput("sick_prob_final", inline = TRUE))
-#           )
-
-#         )
-#       )
-#     ),
-
-#     column(width = 1)  # Right margin
-#   ),
-
-#   # Footer
-# tags$footer(
-#   style = "
-#     background-color: #f0f0f0;
-#     padding: 15px 20px;
-#     margin-top: 30px;
-#     text-align: center;
-#     font-size: 90%;
-#     color: #555;
-#     border-top: 1px solid #ddd;
-#   ",
-#   HTML(paste0(
-#     '<img src="logo_lbbe.svg" alt="LBBE logo" height="30" style="vertical-align: middle; margin-right: 10px;"> &nbsp;|&nbsp; ',
-#     'Source code on <a href="https://github.com/damiendevienne/ftd-als" target="_blank">GitHub</a> &nbsp;|&nbsp; ',
-#     'Licensed under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a> &nbsp;|&nbsp; ',
-#     'Contact: <a href="mailto:dominique.de-vienne@inrae.fr">dominique.de-vienne@inrae.fr</a>'
-#   ))
-# )
-# )
-
-
 
 
 ############
@@ -367,6 +317,31 @@ server <- function(input, output, session) {
   output$age_gap <- renderText({
     req(input$relation == "grandchild", input$age, input$parent_age)
     input$parent_age - input$age
+  })
+
+  #new outputs for summary
+  output$summary_relation <- renderText({
+    if (input$relation == "child") "Child" else "Grandchild"
+  })
+
+  output$summary_age <- renderText({
+    paste(input$age, "years")
+  })
+
+  output$summary_penetrance <- renderText({
+    paste0(round(input$Penetrance * 100), "%")
+  })
+
+  output$summary_parent_age <- renderText({
+    paste(input$parent_age, "years")
+  })
+
+  output$summary_age_gap <- renderText({
+    paste(input$parent_age - input$age, "years")
+  })
+
+  output$summary_nyears <- renderText({
+    paste(input$n_years, "years")
   })
 
   adjusted_df <- reactive({ #this dataframe is updated when variables (n, x, relation) change
@@ -414,31 +389,42 @@ server <- function(input, output, session) {
     round(get_sick_prob(), 2)
   })
 
-  # Server outputs plots using make_plot function
-  output$interactive_plot <- renderPlotly({
-    df <- adjusted_df()
-    make_plot(
+  # Server outputs plots using make_combined_plot function
+  output$combined_plot <- renderPlotly({
+    df <- adjusted_df()   
+    make_combined_plot(
       df = df,
       age_input = input$age,
-      prob_line = get_carrier_prob(),
-      y_col = "PvariantWhenHealthy",
-      line_color = "steelblue",
-      plot_title = "Probability of carrying the c9orf72 RE mutation"
-    )
+      carrier_prob = get_carrier_prob(),
+      sick_prob = get_sick_prob(),
+      nyears = df$n[1]
+      )
   })
 
-  output$interactive_plot2 <- renderPlotly({
-    df <- adjusted_df()
-    make_plot(
-      df = df,
-      age_input = input$age,
-      prob_line = get_sick_prob(),
-      y_col = "PsickInFuture",
-      line_color = "firebrick",
-      plot_title = paste("Probability of developing the disease in the next",df$n[1],"years")
+output$download_report <- downloadHandler(
+  filename = function() {
+    paste0("genetic_report_", Sys.Date(), ".pdf")
+  },
+  content = function(file) {
+    params <- list(
+      relation = input$relation,
+      age = input$age,
+      parent_age = if (input$relation == "grandchild") input$parent_age else NA,
+      penetrance = input$Penetrance,
+      nyears = input$n_years,
+      carrier = get_carrier_prob(),
+      risk = get_sick_prob(),
+      date_time = format(Sys.time(), "%Y-%m-%d %H:%M:%S"), 
+      thedataframe = adjusted_df()
     )
-  })  
 
-}
+    rmarkdown::render(
+      input = "report-template.Rmd",
+      output_file = file,
+      params = params,
+      envir = new.env(parent = globalenv())
+    )
+  }
+)}
 
 shinyApp(ui, server)
